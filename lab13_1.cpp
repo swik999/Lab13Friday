0ccdebec9d0ed3eda9ff4e2bfcea330e42a9b7c4
Suwat Inkaew <swik999@gmail.com>
2021-01-22 16:13:47 +0700

    done

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,6 +3,21 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+	int i, j;
+	double key;
+	for(i = 1; i < N ; i++){
+		key = d[i];
+		j = i-1;
+		while( j >= 0 && d[j] < key){
+			d[j+1] = d[j];
+			j = j-1;
+			
+		}
+		d[j+1]=key;
+		cout << "Pass " << i << ":" ;
+			for(int i = 0; i < 10; i++) cout << d[i] << " "; 
+			cout << "\n" ;
+	}
 
 }
 
